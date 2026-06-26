b3b8a758f656335e8157a8fd6e9e7ab9fc2e3fc5
sauravgupta-blip <sauravpryagraj@gmail.com>
2026-06-26 22:58:23 +0530

    Remove unused axios import from CitizenDashboard

--- frontend/src/pages/CitizenDashboard.js
+++ frontend/src/pages/CitizenDashboard.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'; 
 
 const DEPARTMENTS = {
   'Pothole': { name: 'PWD (Public Works Department)', phone: '1800-XXX-XXXX', email: 'pwd@municipal.gov.in' },
