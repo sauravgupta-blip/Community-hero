98e3e122efa9674295bb06b2eddf4a3a773f8f09
sauravgupta-blip <sauravpryagraj@gmail.com>
2026-06-26 22:38:16 +0530

    Fix imports: Add axios to CitizenDashboard

--- frontend/src/page/CitizenDashboard.js
+++ frontend/src/page/CitizenDashboard.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios'; 
 
 const DEPARTMENTS = {
   'Pothole': { name: 'PWD (Public Works Department)', phone: '1800-XXX-XXXX', email: 'pwd@municipal.gov.in' },
